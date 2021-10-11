d70a386f6f75ab2361980e5ed046a7529f24e53c
alphacpc <alphacpc@gmail.com>
2021-10-11 17:58:20 +0000

    Added Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GADS Angular App';
+
+  products : any[] = [1,2,3,4,5,6];
+  imgWidth : number  = 80;
+  imgHeight : number = 80;
+  showImg : boolean = false;
+
+
+  toggleImage(): void{
+    this.showImg = !this.showImg;
+  }
 }
