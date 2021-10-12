cab875699d82ff81571ddf95878ae4e695a1bea0
alphacpc <alphacpc@gmail.com>
2021-10-12 16:40:41 +0000

    Add dynamic datas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,49 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'GADS Angular App';
 
-  products : any[] = [1,2,3,4,5,6];
+  products : any[] = [
+    {
+      "productId": "5246",
+      "productName": "Chou",
+      "productCode": "PR-5420-L",
+      "productDesc": "Lorem ipsum",
+      "productPrice": "200",
+      "productNote": "4.2",
+      "productImage": "assets/images/chou.png",
+    },
+    {
+      "productId": "5247",
+      "productName": "Tomate",
+      "productCode": "PR-5445-L",
+      "productDesc": "Lorem ipsum",
+      "productPrice": "300",
+      "productNote": "4",
+      "productImage": "assets/images/tomate.png",
+    },
+    {
+      "productId": "5656",
+      "productName": "Kiwi",
+      "productCode": "PR-4558-F",
+      "productDesc": "Lorem ipsum",
+      "productPrice": "250",
+      "productNote": "4",
+      "productImage": "assets/images/kiwi.png",
+    },
+    {
+      "productId": "5657",
+      "productName": "Pasteque",
+      "productCode": "PR-5545-F",
+      "productDesc": "Lorem ipsum",
+      "productPrice": "400",
+      "productNote": "4.8",
+      "productImage": "assets/images/pasteque.png",
+    }
+  ];
+  
   imgWidth : number  = 80;
   imgHeight : number = 80;
   showImg : boolean = false;
-
+  textFilter : string = "";
 
   toggleImage(): void{
     this.showImg = !this.showImg;
